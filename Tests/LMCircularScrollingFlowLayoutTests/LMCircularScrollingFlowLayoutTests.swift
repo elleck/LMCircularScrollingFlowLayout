3cca999e27fbfd6eb1fe85c93bc5723ea4d1704c
Ellech Wiltshire <elleck@elleck.com>
2022-03-21 10:41:20 -0600

    added swift package manager support

---
+++ Tests/LMCircularScrollingFlowLayoutTests/LMCircularScrollingFlowLayoutTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import LMCircularScrollingFlowLayout
+
+final class LMCircularScrollingFlowLayoutTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(LMCircularScrollingFlowLayout().text, "Hello, World!")
+    }
+}
